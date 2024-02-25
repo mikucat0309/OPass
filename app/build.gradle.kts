2d6da4021bf9100a1a5801c490e47990ba9cf0e4
Mikucat <admin@mikucat.me>
2024-02-25 13:45:39 +0800

    chore: upgrade AGP to 8.2.2

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,7 +1,7 @@
 import io.gitlab.arturbosch.detekt.Detekt
 
 plugins {
-  id("com.android.application") version "8.2.1"
+  id("com.android.application") version "8.2.2"
   id("org.jetbrains.kotlin.android") version "1.9.20"
   id("org.jetbrains.kotlin.plugin.serialization") version "1.9.20"
   id("com.google.devtools.ksp") version "1.9.20-1.0.14"
